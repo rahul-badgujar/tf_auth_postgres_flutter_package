3f19e4c8ff9cdef2bd51d9390194eafde7f180cd
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-20 17:16:48 +0530

    impl

--- lib/src/tf_auth_postgres_impl.dart
+++ lib/src/tf_auth_postgres_impl.dart
@@ -1,3 +1,34 @@
 import 'package:tf_auth_base/tf_auth_base.dart';
 
-class TfAuthPostgres extends TfAuth {}
+class TfAuthPostgres extends TfAuth {
+  @override
+  Future<void> loginWithEmailPassword(
+      {required String email, required String password}) {
+    // Postgres specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithEmailLink({required String email}) {
+    // Postgres specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithApple() {
+    // Postgres specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithFacebook() {
+    // Postgres specific implementation here
+    return Future<void>.value();
+  }
+
+  @override
+  Future<void> loginWithGoogle() {
+    // Postgres specific implementation here
+    return Future<void>.value();
+  }
+}

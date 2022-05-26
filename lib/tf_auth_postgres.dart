cc5752a42c5d96ff8f4ab46e3443189bf6e888c0
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-26 15:19:31 +0530

    exporting tf_auth_base

--- lib/tf_auth_postgres.dart
+++ lib/tf_auth_postgres.dart
@@ -1,1 +1,7 @@
 library tf_auth_postgres;
+
+// exporting tf_base for basic data types and interfaces
+export 'package:tf_auth_base/tf_auth_base.dart';
+
+// exporting widgets and impl classes
+export 'src/tf_auth_postgres_impl.dart';

119c16799a2c0829f40ff037207545369acf0934
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-06-01 18:42:18 +0530

    updated for latest changes in tf auth base

--- lib/src/tf_auth_postgres_impl.dart
+++ lib/src/tf_auth_postgres_impl.dart
@@ -2,50 +2,50 @@ import 'package:tf_auth_base/tf_auth_base.dart';
 
 class TfAuthPostgres extends TfAuth {
   @override
-  Future<void> forgotPasswordForEmail({required String email}) {
+  Future<TfAuthUser> forgotPasswordForEmail({required String email}) {
     // TODO: implement forgotPasswordForEmail
     throw UnimplementedError();
   }
 
   @override
-  Future<void> loginWithApple() {
+  Future<TfAuthUser> loginWithApple() {
     // TODO: implement loginWithApple
     throw UnimplementedError();
   }
 
   @override
-  Future<void> loginWithEmailLink({required String email}) {
+  Future<TfAuthUser> loginWithEmailLink({required String email}) {
     // TODO: implement loginWithEmailLink
     throw UnimplementedError();
   }
 
   @override
-  Future<void> loginWithEmailPassword(
+  Future<TfAuthUser> loginWithEmailPassword(
       {required String email, required String password}) {
     // TODO: implement loginWithEmailPassword
     throw UnimplementedError();
   }
 
   @override
-  Future<void> loginWithFacebook() {
+  Future<TfAuthUser> loginWithFacebook() {
     // TODO: implement loginWithFacebook
     throw UnimplementedError();
   }
 
   @override
-  Future<void> loginWithGoogle() {
+  Future<TfAuthUser> loginWithGoogle() {
     // TODO: implement loginWithGoogle
     throw UnimplementedError();
   }
 
   @override
-  Future<void> logout() {
+  Future<TfAuthUser> logout() {
     // TODO: implement logout
     throw UnimplementedError();
   }
 
   @override
-  Future<void> signupWithEmailPassword(
+  Future<TfAuthUser> signupWithEmailPassword(
       {required String email, required String password}) {
     // TODO: implement signupWithEmailPassword
     throw UnimplementedError();
